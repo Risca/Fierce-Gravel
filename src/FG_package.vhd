ea11128796a8af85c1c498a4ebae29fa06827324
Patrik Dahlström <patrikd@telia.com>
2011-12-03 18:46:55 +0100

    Added type 'byte'
    Signed-off-by: Patrik Dahlström <patrikd@telia.com>

--- src/FG_package.vhd
+++ src/FG_package.vhd
@@ -3,8 +3,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package resources is
+	type byte is array (7 downto 0) of std_logic;
 	-- To index column Foo: Foo(byte)(bit);
-	type column is array (3 downto 0) of std_logic_vector(7 downto 0);
+	type column is array (3 downto 0) of byte;
 	-- To index state_array Foo: Foo(column)(byte)(bit);
 	type state_array is array (3 downto 0) of column;
 
@@ -20,6 +21,7 @@ package resources is
 	end component add_round_key;
 
 	-- Overloaded operators
+	function "xor" (L,R : byte) return byte;
 	function "xor" (L,R : column) return column;
 	function "xor" (L,R : state_array) return state_array;
 end package resources;
@@ -28,6 +30,12 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package body resources is
+	function "xor" (L,R : byte) return byte is
+		variable result : byte;
+	begin
+		return (L xor R);
+	end function;
+
 	function "xor" (L,R : column) return column is
 		variable result : column;
 	begin
