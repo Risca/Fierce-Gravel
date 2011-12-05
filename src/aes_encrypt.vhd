9423bba94805c64a8d61f3d0ea3345338b4232ec
Staffan Sjöqvist <stafan87@gmail.com>
2011-12-05 22:52:07 +0100

    It works...
    Signed-off-by: Staffan Sjöqvist <stafan87@gmail.com>

--- src/aes_encrypt.vhd
+++ src/aes_encrypt.vhd
@@ -7,25 +7,26 @@ use work.resources.all;
 entity aes_encrypt is
 	port(	PLAINTEXT	: in  state_array;
 			CIPHERKEY	: in  cipher_key;
-			CPHERTEXT	: out state_array);
+			CIPHERTEXT	: out state_array);
 end aes_encrypt;
 
 architecture structure of aes_encrypt is
 	type state_t is array(0 to (Nr-1)) of state_array;
-	signal after_main_rounds : state_array;
+--	signal after_main_rounds : state_array;
 	signal state : state_t;
 	signal round_keys : round_keys_t;
 	
-begin
 	
+	
+	signal after_first: state_array;
+begin
 	KE: key_expansion port map(INPUT => CIPHERKEY, OUTPUT => round_keys);
 	U0: first_round port map(plaintext_in => PLAINTEXT, cipherkey_in => round_keys(0), output => state(0));
-	
-	-- Generate (Nr-1) main rounds
+
+	--	 Generate (Nr-1) main rounds
 	V1: for i in 1 to (Nr-1) generate
 		MR: main_round port map(state_in => state(i-1), roundkey_in => round_keys(i), state_out => state(i) );		
 	end generate;
-	
-	U1: final_round port map(state_in => state(Nr-1), roundkey_in => round_keys(Nr), state_out => after_main_rounds );		
 
+	U1: final_round port map(state_in => state(Nr-1), roundkey_in => round_keys(Nr), state_out => CIPHERTEXT );	
 end structure;
