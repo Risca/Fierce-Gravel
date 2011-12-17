12c7a1caaf2a908da5d225b1cbd4c3d09021ed82
Daniel Josefsson <dannejosefsson@gmail.com>
2011-12-17 18:36:54 +0100

    Added work.variables.all to dependency list.
    Signed-off-by: Daniel Josefsson <dannejosefsson@gmail.com>

--- src/cipher_key_hard_coded.vhd
+++ src/cipher_key_hard_coded.vhd
@@ -2,6 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.resources.all;
+use work.variables.all;
 -- Cipher key test vectors.
 -- 2011-12-05
 -- Author:	Daniel Josefsson and Patrik Dahlstrm
