921da6af0e6e098a851e1f3b2d6397c47710845e
Daniel Josefsson <dannejosefsson@gmail.com>
2011-12-17 20:01:52 +0100

    inv_main_round is now working with all its components.
    Signed-off-by: Daniel Josefsson <dannejosefsson@gmail.com>

--- src/inv_main_round.vhd
+++ src/inv_main_round.vhd
@@ -10,12 +10,11 @@ end inv_main_round;
 
 architecture structure of inv_main_round is
 
-signal after_sbox, after_shiftRows, after_mixColumns : state_array;
+signal after_sbox, after_shiftRows, after_add_round_key : state_array;
 
 begin
-	U0: inv_shift_rows		port map(INPUT => state_in, OUTPUT => state_out); -- after_shiftRows
-	-- sbox 			port map(INPUT => state_in, OUTPUT => after_sbox);
-	-- U1: 
-	-- U2: mix_columns	port map(INPUT => after_shiftRows, OUTPUT => after_mixColumns);
-	-- U3: add_round_key	port map(INPUT => after_mixColumns, KEY => roundkey_in, OUTPUT => state_out);
+	U0: inv_shift_rows	port map(INPUT => state_in, OUTPUT => after_shiftRows);
+	U1: inv_sbox 			port map(INPUT => after_shiftRows, OUTPUT => after_sbox); -- 
+	U2: add_round_key		port map(INPUT => after_sbox, KEY => roundkey_in, OUTPUT => after_add_round_key); -- state_out
+	U3: inv_mix_columns	port map(INPUT => after_add_round_key, OUTPUT => state_out);
 end structure;
