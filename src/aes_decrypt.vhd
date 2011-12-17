47f28afeaa35b43fd17848e242e6b26e24a47e99
Daniel Josefsson <dannejosefsson@gmail.com>
2011-12-17 14:39:37 +0100

    Begun the main rounds implemention.
    Signed-off-by: Daniel Josefsson <dannejosefsson@gmail.com>

--- src/aes_decrypt.vhd
+++ src/aes_decrypt.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use work.variables.all;
 use work.resources.all;
 
 -- Top level entity for the Fierce-Gravel AES decryptor
@@ -21,13 +22,14 @@ architecture structure of aes_decrypt is
 	signal after_first: state_array;
 begin
 	KE: key_expansion port map(INPUT => CIPHERKEY, OUTPUT => round_keys);
-	U0: inv_first_round port map(ciphertext_in => CIPHERTEXT, cipherkey_in => round_keys(Nr), output => PLAINTEXT); -- state(0)
+	U0: inv_first_round port map(ciphertext_in => CIPHERTEXT, cipherkey_in => round_keys(Nr), output => state(0)); -- PLAINTEXT
 	
 	--	 Generate (Nr-1) main rounds
 	-- Uncomment to add later rounds.
---	V1: for i in 1 to (Nr-1) generate
---		MR: inv_main_round port map(state_in => state(i-1), roundkey_in => round_keys(Nr - 1), state_out => state(i) );		
---	end generate;
+	-- V1: for i in 1 to 1 generate -- (Nr-1)
+	 MR: inv_main_round port map(state_in => state(0), roundkey_in => round_keys(Nr - 1), state_out =>  PLAINTEXT); --state(i)
+		-- MR: inv_main_round port map(state_in => state(i-1), roundkey_in => round_keys(Nr - i), state_out =>  PLAINTEXT); --state(i)
+	-- end generate;
 --
 --	U1: inv_final_round port map(state_in => state(Nr-1), roundkey_in => round_keys(0), state_out => PLAINTEXT );
 end structure;
